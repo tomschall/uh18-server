8492415709f827bbaf4514b4a60436ef6235de9c
Adrian <comixtrip@treiet61.at>
2018-05-04 16:00:17 +0200

    basic server classes for websocket and osc

---
+++ server.js
@@ -0,0 +1,31 @@
+/* UH18 #shipbeat - OSC2MIDI
+ *
+ * This app listens to a UDP port for OSC messages.
+ * The messages are forwarded via Websockets to the UI and,
+ * if any configuration found for a OSC message in mongodb,
+ * the messages are converted to MIDI and sent to a MIDI port.
+ *
+ * Usage:
+ * Start application: `node server.js`
+ *
+ * @todo: Startup options:
+ * e.g.: -p Specifies the UDP port to listen for OSC messages
+ */
+
+"use strict";
+
+const wsServer = require("./lib/WebsocketServer");
+const oscServer = require("./lib/OscServer");
+
+/**
+ * Midi Mapper
+ */
+
+/**
+ * Tests
+ */
+
+let wsTest = new wsServer.WebsocketServer();
+console.log(wsTest);
+let oscServerTest = new oscServer.OscServer();
+console.log(oscServerTest);
